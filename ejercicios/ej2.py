ea2bdac5746f007a4ad3487b68e40954bba98848
niicooam12 <nico.amador.montesinos@gmail.com>
2025-04-04 09:25:07 +0200

    Cambiado y añadido matriz iteratio y recursivo

---
+++ ejercicios/ej2.py
@@ -0,0 +1,17 @@
+class Matriz:
+    def __init__(self, matriz):
+        self.matriz = matriz
+
+    def calcular_determinante_recursivo(self):
+        return self._determinante_recursivo(self.matriz)
+
+    def _determinante_recursivo(self, matriz):
+        if len(matriz) != 3 or any(len(fila) != 3 for fila in matriz):
+            return("La matriz debe ser de 3x3")
+        
+        # Fórmula para el determinante de una matriz 3x3
+        return (
+            matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1]) -
+            matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0]) +
+            matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[1][1] * matriz[2][0])
+        )
